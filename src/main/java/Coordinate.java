c1733ec5b24412d45823172890571426bf0f7552
GrossMarie <Gross.Marie@yandex.ru>
2015-10-08 02:30:33 +0300

    - first working release

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -4,8 +4,13 @@ public class Coordinate {
 
     public Coordinate(String s) {
         String[] split = s.split(",");
-        latitude = Double.parseDouble(split[1]);
-        longitude = Double.parseDouble(split[0]);
+        latitude = Double.parseDouble(split[1])+ 180;
+        longitude = Double.parseDouble(split[0])+ 90;
+    }
+
+    public Coordinate(int latitude, int longitude) {
+        this.latitude = latitude ;
+        this.longitude = longitude  ;
     }
 
     @Override
@@ -17,10 +22,37 @@ public class Coordinate {
     }
 
     public double getLat() {
-        return latitude + 180;
+        return latitude ;
     }
 
     public double getLong() {
-        return longitude + 90;
+        return longitude;
+    }
+
+    public static Coordinate roundTo(Coordinate coordinate, int roundTo) {
+        return new Coordinate((int)(coordinate.latitude / roundTo) * roundTo, (int)( coordinate.longitude / roundTo) * roundTo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinate that = (Coordinate) o;
+
+        if (Double.compare(that.latitude, latitude) != 0) return false;
+        return Double.compare(that.longitude, longitude) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -1,16 +1,23 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Mapper {
 
-    public static final int MULTIPLIER = 30;
+    public static final int MULTIPLIER = 10;
     public static final int WIDTH = 360 * MULTIPLIER;
     public static final int HEIGHT = 180 * MULTIPLIER;
+    public static final int ROUND_TO = 20;
 
     public void start() throws IOException {
         System.out.println("Start Mapper");
@@ -27,14 +34,47 @@ public class Mapper {
                         BufferedImage.TYPE_INT_ARGB);
 
         Graphics2D g2 = outputImage.createGraphics();
-        g2.setColor(Color.red);
 
         g2.drawImage(map, 0, 0, WIDTH, HEIGHT, (img, f, x1, y1, width, height) -> true);
 
-        Files.lines(file.toPath()).map(Coordinate::new).forEach(coordinate -> {
+        g2.setColor(Color.red);
+        Stream<Coordinate> coordinateStream = Files.lines(file.toPath()).map(Coordinate::new);
+        java.util.List<Coordinate> coordinates = coordinateStream.collect(Collectors.toList());
+        coordinates.forEach(coordinate -> {
             int x = (int) ((coordinate.getLat()) * MULTIPLIER);
             int y = (int) ((coordinate.getLong()) * MULTIPLIER);
             g2.drawRect(x, HEIGHT - y, 5, 5);
+        });
+
+        g2.setColor(Color.green);
+        g2.setStroke(new BasicStroke(3));
+
+        HashMap<Coordinate, Integer> block = new HashMap<>();
+
+        coordinates.forEach(c -> {
+            c = Coordinate.roundTo(c, ROUND_TO);
+
+            Integer value = block.get(c);
+            if (value == null) {
+                block.put(c, 1);
+            } else {
+                block.put(c, 1 + value);
+            }
+        });
+
+        g2.setColor(Color.blue);
+        BasicStroke stroke = new BasicStroke(3);
+        g2.setStroke(stroke);
+        int fontSize = 5 * MULTIPLIER;
+        g2.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+
+        block.entrySet().forEach(entry -> {
+            Coordinate c = entry.getKey();
+            int width = ROUND_TO * MULTIPLIER;
+            int x = (int) ((c.getLat()) * MULTIPLIER);
+            int y = HEIGHT - (int) ((c.getLong()) * MULTIPLIER) - width;
+            g2.drawRect(x, y, width, width);
+            g2.drawString(entry.getValue() + "", x + fontSize * 0.2f, y + fontSize * 0.8f);
         });
 
         File outputFile = new File("output.png");
